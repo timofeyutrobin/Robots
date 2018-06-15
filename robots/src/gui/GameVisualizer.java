4329b155a9766809a0e7019a4f550c57bef5ae5a
Timofey Utrobin <null>
2018-06-15 15:16:59 +0500

    Game map file and directory is creating if not exists

--- robots/src/gui/GameVisualizer.java
+++ robots/src/gui/GameVisualizer.java
@@ -10,6 +10,9 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -18,6 +21,7 @@ import javax.swing.JPanel;
 public class GameVisualizer extends JPanel
 {
     static final String GAME_MAP_FILE = "robots/data/game.dat";
+    static final String GAME_MAP_DIRECTORY = "robots/data";
 
     public enum MouseMode {
         SET_TARGET_MODE,
@@ -104,8 +108,18 @@ public class GameVisualizer extends JPanel
     }
 
     void save() {
+        Path filePath = Paths.get(GAME_MAP_FILE);
+        Path dirPath = Paths.get(GAME_MAP_DIRECTORY);
+        if (!Files.exists(filePath)) {
+            try {
+                Files.createDirectory(dirPath);
+                Files.createFile(filePath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         File file = new File(GAME_MAP_FILE);
-        try(FileOutputStream fos = new FileOutputStream(file); ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+        try (FileOutputStream fos = new FileOutputStream(file); ObjectOutputStream oos = new ObjectOutputStream(fos)) {
             oos.writeObject(gameMap);
             oos.flush();
         }
